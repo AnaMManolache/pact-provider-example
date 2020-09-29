f74771bb2a1b208120888f71f9e9e85bc30ef1e7
Ana-Maria (A.) Manolache <ana-maria.manolache@ing.com>
2020-09-29 15:45:32 +0200

    Remove property setting

--- src/test/java/nl/ing/cdc/example/calculator/CalculatorBrokerPactTest.java
+++ src/test/java/nl/ing/cdc/example/calculator/CalculatorBrokerPactTest.java
@@ -8,11 +8,9 @@ import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import org.apache.http.HttpRequest;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -28,24 +26,6 @@ public class CalculatorBrokerPactTest {
     void setTarget(PactVerificationContext context) {
         HttpTestTarget target = new HttpTestTarget("localhost", 8081);
         context.setTarget(target);
-    }
-
-    @BeforeAll
-    static void pactBrokerSetup(@Value("${pactbroker.host}") String host,
-                                @Value("${pactbroker.port}") String port,
-                                @Value("${pactbroker.scheme}") String scheme,
-                                @Value("${pactbroker.auth.username}") String username,
-                                @Value("${pactbroker.auth.password}") String password,
-                                @Value("${pact.verifier.publishResults}") String publishResults,
-                                @Value("${pact.consumer.version}") String pactConsumerVersion) {
-        System.setProperty("pact.verifier.publishResults", publishResults);
-        System.setProperty("pact.consumer.version", pactConsumerVersion);
-
-        System.setProperty("pactbroker.host", host);
-        System.setProperty("pactbroker.port", port);
-        System.setProperty("pactbroker.scheme", scheme);
-        System.setProperty("pactbroker.auth.username", username);
-        System.setProperty("pactbroker.auth.password", password);
     }
 
     @TestTemplate
