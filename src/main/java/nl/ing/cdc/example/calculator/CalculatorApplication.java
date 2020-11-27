24901ffe42894bed7ded88bbc100aad4966aca1d
Ana-Maria (A.) Manolache <ana-maria.manolache@ing.com>
2020-11-27 10:31:27 +0100

    Set pact provider version

--- src/main/java/nl/ing/cdc/example/calculator/CalculatorApplication.java
+++ src/main/java/nl/ing/cdc/example/calculator/CalculatorApplication.java
@@ -2,12 +2,26 @@ package nl.ing.cdc.example.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocations(new ClassPathResource("git.properties"), new ClassPathResource("/META-INF/build-info.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
     }
 
 }
--- src/test/java/nl/ing/cdc/example/calculator/CalculatorBrokerPactTest.java
+++ src/test/java/nl/ing/cdc/example/calculator/CalculatorBrokerPactTest.java
@@ -22,12 +22,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(properties = "server.port=8081")
 @PactBroker(scheme = "https", host = "${PACT_BROKER_HOST}",
-        enablePendingPacts = "#{systemProperties['PACT_ENABLE_PENDING'] ?: 'false'",
+        enablePendingPacts = "${PACT_ENABLE_PENDING},",
         authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 @ExtendWith(SpringExtension.class)
 @Provider("CalculationAPI")
 @IgnoreNoPactsToVerify
-public class CalculatorBrokerPactTest {
+class CalculatorBrokerPactTest {
 
     @BeforeEach
     void setTarget(PactVerificationContext context) {
@@ -39,9 +39,18 @@ public class CalculatorBrokerPactTest {
     }
 
     @BeforeAll
-    static void pactBrokerSetup(@Value("${pact.provider.version}") String pactProviderVersion) {
-        System.setProperty("pact.verifier.publishResults", "true");
-        System.setProperty("pact.provider.version", pactProviderVersion);
+    static void pactBrokerSetup(@Value("${pact_verifier_publish:false}") String publishResults,
+                                @Value("${CONSUMER_TAGS:}") String consumerTags,
+                                @Value("${build.version:unknown}") String pactProviderVersion,
+                                @Value("${pact_consumer_version:unknown}") String pactConsumerVersion,
+                                @Value("${git.commit.id.abbrev}") String gitCommitIdShort) {
+        System.setProperty("pact.verifier.publishResults", publishResults);
+        System.setProperty("pact.provider.version", String.format(pactProviderVersion, '-', gitCommitIdShort));
+        System.setProperty("pact.consumer.version", pactConsumerVersion);
+
+        if (consumerTags != null && !consumerTags.isEmpty()) {
+            System.setProperty("pactbroker.tags", consumerTags);
+        }
     }
 
     @TestTemplate
